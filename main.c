e964eb810f0af114d146347008835206cff663a4
taubin17 <tyler.aubin@maine.edu>
2020-02-26 15:46:00 -0500

    Main.c altered to print return of mmult pointer

--- main.c
+++ main.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "matrix.h"
-
+#include <math.h>
+#include <time.h>
+#include "mmult.h"
+//Program takes in 4 size parameters, creates matricies of parameter sizes, and then prints the matrix product (in addition prints time to execute).
 int main (int argc, char *argv[])
-{
-	struct dimensions a, b;
-	a = mat(argv[1]);
-	b = mat(argv[2]);
-	
-	printf("%d\n%d\n", a.rows, b.rows);	
+{	
+	//Future built matrices
+	struct matrix mat1;
+	struct matrix mat2;
+	//Time logs
+	struct timespec start, finish;
+	struct matrix *result;
+
+	//Matrix Dimensions:
+	int row1 = atoi(argv[1]);
+	int row2 = atoi(argv[3]);
+	int column1 = atoi(argv[2]);
+	int column2 = atoi(argv[4]);
+	//Building Arrays based on parameter size
+	mat1 = mat(row1, column1);
+	mat2 = mat(row2, column2);
+	//Log mmult start time
+	clock_gettime(CLOCK_REALTIME, &start);
+	result = mmult(&mat1, &mat2);
+	//Log mmult end time
+	clock_gettime(CLOCK_REALTIME, &finish);
+	printf("Time in Seconds: %ld.%ld\n", (finish.tv_sec - start.tv_sec), (finish.tv_nsec - start.tv_nsec));
+	free(result);
 	return 0;	
 }
-
