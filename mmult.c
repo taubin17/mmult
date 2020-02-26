1423f9e3c0be64a96082dcb8710635f21e6f4efa
taubin17 <tyler.aubin@maine.edu>
2020-02-26 15:45:42 -0500

    MMult now updated to contain all functions involivng matrix

--- mmult.c
+++ mmult.c
@@ -1,29 +1,57 @@
 #include <stdio.h>
-#include "matrix.h"
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "mmult.h"
 
-int *multiply (struct matrix *one, struct matrix *two)
+double rand_double() //PRE: Rand is random integer number. POST: Rand now is a number between 0 and 100
 {
-	
-	int temp = 0;
-	int result[one->columns][two->rows];
-
-		
-	
-	for (int i = 0; i < one->rows; i++) {
-		for (int j = 0; j < two->columns; j++) {
-			for (int k = 0; k < one->columns; k++) {
-				temp += one->elements[i][k] * two->elements[k][j];
+	const double max = 100.0;
+	double result = ((double)rand() / (double)RAND_MAX) * max;
+	return result;
+}
+struct matrix *mmult (struct matrix *mat1, struct matrix *mat2) //PRE: Matrix 
+{	
+	struct matrix* result = (struct matrix *)malloc(sizeof(struct matrix));
+	result->elements = (double **)malloc(mat1->rows * sizeof(double *));
+	for (int i = 0; i < mat1->columns; i++) {
+		result->elements[i] = (double *)malloc(mat2->columns * sizeof(double));
+	}
+	double temp = 0;
+	for (int i = 0; i < mat1->rows; i++) {
+		for (int j = 0; j < mat2->columns; j++) {
+			for (int k = 0; k < mat2->rows; k++) {
+				temp += (mat1->elements[i][k] * mat2->elements[k][j]);
 			}
-			result[i][j] = temp;
-			temp = 0;
+			result->elements[i][j] = temp;
+			temp = 0;	
 		}
 	}
-	
-	for (int i = 0; i < one->columns; i++) {
-		for (int j = 0; j < two->rows; j++) {
-			printf("%d ", result[i][j]);
+	/*for (int i = 0; i < mat1->rows; i++) {
+		for (int j = 0; j < mat2->columns; j++) {
+			printf("%lf ", result->elements[i][j]);
 		}
 		printf("\n");
-	}
-	return *result;
+	} */
+	return result; 
 }
+
+struct matrix mat(int rows, int columns) 
+{	
+	struct matrix mat;
+	mat.rows = rows;
+	mat.columns = columns;
+	mat.elements = (double **)malloc(rows * sizeof(double *));
+	for (int i = 0; i < mat.rows; i++) {
+		mat.elements[i] = (double *)malloc(columns * sizeof(double));
+	}
+	for (int i = 0; i < mat.rows; i++) {
+		for (int j = 0; j < mat.columns; j++) {
+			//mat.elements[i][j] = rand_double();
+			mat.elements[i][j] = 1;
+		}
+	}
+	return mat;
+}
+
